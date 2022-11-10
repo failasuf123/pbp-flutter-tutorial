35dcf0065546b19561fb7e8e0c89555451420e1e
Failasuf Indi Marsendy <failasufindi123@gmail.com>
2022-11-10 09:39:02 +0700

    new task 1

---
+++ lib/form.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_app/main.dart';
+import 'package:flutter/material.dart';
+
+class MyFormPage extends StatefulWidget {
+  const MyFormPage({super.key});
+
+  @override
+  State<MyFormPage> createState() => _MyFormPageState();
+}
+
+class _MyFormPageState extends State<MyFormPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Form'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Hello World!'),
+          ],
+        ),
+      ),
+    );
+  }
+}
